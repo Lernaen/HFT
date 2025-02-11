b622a9ab0634a476ad72f167a214cc3a0fc0ae12
Kiera Furneaux <buffshoujo@gmail.com>
2025-02-11 22:32:44 +1300

    Client side APi built

--- server/db/JobsDb.ts
+++ server/db/JobsDb.ts
@@ -2,7 +2,18 @@ import connection from './connection.ts'
 import { Job, NewJob } from '../../models/jobsModel.ts'
 
 export async function getAllJobs(db = connection): Promise<Job[]> {
-  return db('Jobs').select()
+  return db('Jobs').select(
+    'jobDate',
+    'product',
+    'applicationRate',
+    'pickup',
+    'docketNumber',
+    'orderNumber',
+    'truckNumber',
+    'driver',
+    'cropType',
+    'reasonsForFailure',
+  )
 }
 
 export async function addJob(newjob: NewJob) {
