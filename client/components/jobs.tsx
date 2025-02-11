141f51085ae7c40a9e302d5e6452e816a995dcaf
Kiera Furneaux <buffshoujo@gmail.com>
2025-02-11 23:46:59 +1300

    Need to get the front end to handle the back end

---
+++ client/components/jobs.tsx
@@ -0,0 +1,34 @@
+import { useQuery } from '@tanstack/react-query'
+import { getAllJobs } from '../apis/jobs'
+import DropButton from './DropButton.tsx'
+
+function Joblist() {
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['jobs'],
+    queryFn: () => getAllJobs(),
+  })
+
+  if (isPending) {
+    return <p>Fetching</p>
+  }
+
+  if (isError) {
+    console.error(error.message)
+    return <p>There has been a messup</p>
+  }
+
+  return (
+    <>
+      <div>
+        <p>List Of Jobs</p>
+        <DropButton>
+          {data?.map((job) => {
+            return <li key={job.id}>{job.jobDate}</li>
+          })}{' '}
+        </DropButton>
+      </div>
+    </>
+  )
+}
+
+export default Joblist
