b349c73feac460c6e18711bc32349d0516741dd5
Kiera Furneaux <buffshoujo@gmail.com>
2025-02-13 18:32:02 +1300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://hammondferttracker.vercel.app',
+      '/api': 'https://hammondferttracker.vercel.app',
     },
   },
 })
