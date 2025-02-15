d43452d1717ff470b202bddfc0b91fccd8fff1fe
Kiera Furneaux <buffshoujo@gmail.com>
2025-02-15 16:40:11 +1300

    possible splitting?

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://hammondferttracker.vercel.app',
+      '/api': 'https://fert-tracker-server.vercel.app',
     },
   },
 })
