dca74ffa0f6f3e1d23fa2e906bc863c3d68de1c2
Kiera Furneaux <buffshoujo@gmail.com>
2025-02-16 02:24:58 +1300

    changed vite config.js from localhost

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
+      '/api': 'https://hammond-fertiliser-tracker-21b964005429.herokuapp.com/',
     },
   },
 })
